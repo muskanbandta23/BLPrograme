79a5bd27fb1c7ad080f54816a3a113ed23828760
Muskan Bandta <muskanbandta1@gmail.com>
2025-07-14 12:25:18 +0530

    initial commit

---
+++ untitled/src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Name : Muskan Bandta");
+        System.out.println("Roll No. : 2210991947");
+        System.out.println("Fatrher's Naame : Prem Prakash Bandta");
+        System.out.println("Mother's Name: Kamlesh Bandta");
+        System.out.println("Session: 2022 - 2026");
+        System.out.println("Blood Group: A+");
+    }
+}
